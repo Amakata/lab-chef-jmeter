2c14f1f2e61c2ac1cf87cd4dfadc965b862fc8fe
bageljp <kadoyama.keisuke@gmail.com>
2013-06-11 02:31:59 +0900

    add server recipe.

---
+++ recipes/server.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook Name:: jmeter
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "jmeter::default"
+
+template "/etc/init.d/jmeter-server" do
+  owner node['jmeter']['user']
+  group node['jmeter']['group']
+  mode 00755
+end
+
+service 'jmeter-server' do
+  supports :status => true, :restart => true, :reload => false
+  action [ :enable, :start ]
+end
